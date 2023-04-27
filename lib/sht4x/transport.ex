5b3f0c66a270c8e7034a87dba149d547ab1fb3fc
Masatoshi Nishiguchi <mnishiguchi@users.noreply.github.com>
2023-04-26 23:21:46 -0400

    test: loosen type check for I2C return value

--- lib/sht4x/transport.ex
+++ lib/sht4x/transport.ex
@@ -4,17 +4,28 @@ defmodule SHT4X.Transport do
   use TypedStruct
 
   typedstruct do
-    field(:address, 0..127, enforce: true)
-    field(:ref, reference, enforce: true)
+    field(:address, Circuits.I2C.address(), enforce: true)
+    # We want to allow ref to be of any type so that we can use sim etc as needed.
+    field(:ref, any, enforce: true)
     field(:retries, non_neg_integer, default: 0)
     field(:read_fn, (pos_integer -> {:ok, binary} | {:error, any}), enforce: true)
     field(:write_fn, (iodata -> :ok | {:error, any}), enforce: true)
     field(:write_read_fn, (iodata, pos_integer -> {:ok, binary} | {:error, any}), enforce: true)
   end
 
-  @spec new(reference, 0..127, non_neg_integer) :: {:ok, t()} | {:error, any}
-  def new(ref, address, retries)
-      when is_reference(ref) and is_integer(address) and is_integer(retries) do
+  @spec new(String.t(), Circuits.I2C.address(), non_neg_integer) :: {:ok, t()} | {:error, any}
+  def new(bus_name, address, retries)
+      when is_binary(bus_name) and is_integer(address) and is_integer(retries) do
+    case Circuits.I2C.open(bus_name) do
+      {:error, err} ->
+        {:error, err}
+
+      {:ok, ref} ->
+        ref_to_struct(ref, address, retries)
+    end
+  end
+
+  defp ref_to_struct(ref, address, retries) when is_integer(address) and is_integer(retries) do
     opts = [retries: retries]
 
     {:ok,
@@ -26,17 +37,5 @@ defmodule SHT4X.Transport do
        write_fn: &Circuits.I2C.write(ref, address, &1, opts),
        write_read_fn: &Circuits.I2C.write_read(ref, address, &1, &2, opts)
      }}
-  end
-
-  @spec new(binary, 0..127, non_neg_integer) :: {:ok, t()} | {:error, any}
-  def new(bus_name, address, retries)
-      when is_binary(bus_name) and is_integer(address) and is_integer(retries) do
-    case Circuits.I2C.open(bus_name) do
-      {:error, err} ->
-        {:error, err}
-
-      {:ok, ref} ->
-        new(ref, address, retries)
-    end
   end
 end
