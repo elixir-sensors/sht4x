cb10f788f9cfd79a12c481e90e9c9a87f987dadc
Digit <aaron.shea@smartrent.com>
2023-07-12 15:54:39 -0700

    Don't define defaults in the typedstruct def, fix incorrect documentation for options

--- lib/sht4x/transport.ex
+++ lib/sht4x/transport.ex
@@ -7,7 +7,7 @@ defmodule SHT4X.Transport do
     field(:address, Circuits.I2C.address(), enforce: true)
     # We want to allow ref to be of any type so that we can use sim etc as needed.
     field(:ref, any, enforce: true)
-    field(:retries, non_neg_integer, default: 0)
+    field(:retries, non_neg_integer, enforce: true)
     field(:read_fn, (pos_integer -> {:ok, binary} | {:error, any}), enforce: true)
     field(:write_fn, (iodata -> :ok | {:error, any}), enforce: true)
     field(:write_read_fn, (iodata, pos_integer -> {:ok, binary} | {:error, any}), enforce: true)
