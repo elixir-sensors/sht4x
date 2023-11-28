fd0fe2b156d126926a7ae252bec6b146b0528dc6
Digit <aaron.shea@smartrent.com>
2023-11-27 16:13:10 -0800

    Add code to detect potential bad values from the sensor, and invalid ranges (see: #29)

--- lib/sht4x/measurement.ex
+++ lib/sht4x/measurement.ex
@@ -2,8 +2,17 @@ defmodule SHT4X.Measurement do
   @moduledoc """
   One sensor measurement
   """
+  require Logger
 
   use TypedStruct
+
+  # Raw readings that, when converted, equate to the min and max operating ranges of Rh and temp
+  # [0 - 100] for Rh and [-40 - 125] for temp
+  @min_range_rh 0x0C4A
+  @max_range_rh 0xD914
+
+  @min_range_t 0x0751
+  @max_range_t 0xF8AD
 
   typedstruct do
     field(:dew_point_c, float)
@@ -22,9 +31,28 @@ defmodule SHT4X.Measurement do
   interpreted temperature and humidity.  It does not apply any compensation so
   this is real temperature and humidity detected.
   """
-  @spec from_raw(<<_::48>>) :: t()
   def from_raw(<<raw_t::16, _crc1, raw_rh::16, _crc2>>) do
     timestamp_ms = System.monotonic_time(:millisecond)
+
+    if raw_reading_valid?(raw_t, raw_rh) do
+      make_measurement(raw_t, raw_rh, timestamp_ms)
+    else
+      # Raw readings invalid, don't even attempt to convert them
+      Logger.warning("Your sensor is returning values that could indicate it is damaged!")
+
+      __struct__(
+        temperature_c: 0.0,
+        humidity_rh: 0.0,
+        dew_point_c: 0.0,
+        raw_reading_temperature: raw_t,
+        raw_reading_humidity: raw_rh,
+        timestamp_ms: timestamp_ms,
+        quality: :unusable
+      )
+    end
+  end
+
+  defp make_measurement(raw_t, raw_rh, timestamp_ms) do
     temperature_c = temperature_c_from_raw(raw_t)
     humidity_rh = humidity_rh_from_raw(raw_rh)
 
@@ -46,4 +74,16 @@ defmodule SHT4X.Measurement do
   defp temperature_c_from_raw(raw_t) do
     -45 + 175 * raw_t / (0xFFFF - 1)
   end
+
+  # Function to check the raw values read from the sensor
+  # A few bad values are known: 0x8000 and 0x8001 (according to Sensirion)
+  defp raw_reading_valid?(0x8000, 0x8000), do: false
+  defp raw_reading_valid?(0x8001, 0x8000), do: false
+
+  # Ensure raw values would be within min/max operating ranges
+  defp raw_reading_valid?(raw_t, raw_rh)
+       when raw_rh not in @min_range_rh..@max_range_rh or raw_t not in @min_range_t..@max_range_t,
+       do: false
+
+  defp raw_reading_valid?(_raw_t, _raw_rh), do: true
 end
--- test/sht4x/measurement_test.exs
+++ test/sht4x/measurement_test.exs
@@ -7,11 +7,32 @@ defmodule SHT4X.MeasurementTest do
   test "converts raw measurement" do
     result = Measurement.from_raw(<<101, 233, 234, 109, 229, 160>>)
 
+    assert result.quality == :fresh
     assert result.raw_reading_temperature == 26_089
     assert result.raw_reading_humidity == 28_133
 
     assert_in_delta result.temperature_c, 24.67, 0.01
     assert_in_delta result.humidity_rh, 47.66, 0.01
     assert_in_delta result.dew_point_c, 12.82, 0.01
+  end
+
+  test "detects possible damaged sensor by looking for 0x8000 in both RH and Temp values" do
+    result = Measurement.from_raw(<<128, 0, 162, 128, 0, 162>>)
+    assert result.quality == :unusable
+  end
+
+  test "detects possible damaged sensor by looking for 0x8001 in raw Temp value, and 0x8000 in raw Rh value" do
+    result = Measurement.from_raw(<<128, 1, 162, 128, 0, 162>>)
+    assert result.quality == :unusable
+  end
+
+  test "detects possible damaged sensor by looking for values outside of operating range (Rh)" do
+    result = Measurement.from_raw(<<101, 233, 234, 0xFF, 0xFF, 172>>)
+    assert result.quality == :unusable
+  end
+
+  test "detects possible damaged sensor by looking for values outside of operating range (C)" do
+    result = Measurement.from_raw(<<0xFF, 0xFF, 172, 109, 229, 160>>)
+    assert result.quality == :unusable
   end
 end
